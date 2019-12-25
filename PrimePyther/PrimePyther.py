b755a105d2bae0caa5ad6ac6161c853cb33263f0
Marcin Gawlik <marcin.gawlik@windowslive.com>
2019-12-25 22:13:39 +0100

    Added some debug prints and linear factor calculation.

--- PrimePyther/PrimePyther.py
+++ PrimePyther/PrimePyther.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 
 found_primes = [1, 2]
+linear_factor_sum = 2.0  # 1/1 + 2/2
 current_value = 3
 last_tstamp = datetime.now()
-
-# print("      1 : 1")
-# print("      2 : 2")
 
 while True:
     found_divider = False
@@ -20,12 +18,18 @@ while True:
             break
 
     if not found_divider:
-        # print(f"{len(found_primes):7} : {current_value}")
         found_primes.append(current_value)
+        linear_factor_sum = linear_factor_sum + (current_value / len(found_primes))
+
+        if (len(found_primes) < 20):
+            print(f"{len(found_primes):7} : {current_value:8} ; l. factor sum : {linear_factor_sum:10.5f}")
 
         if len(found_primes) % 10000 == 0:
             now_tstamp = datetime.now()
-            print(f"{len(found_primes):7} @ {now_tstamp} : {current_value:8} took {now_tstamp - last_tstamp}")
+            print(f"{len(found_primes):7} @ {now_tstamp} : "
+                  f"{current_value:8} took {now_tstamp - last_tstamp} ; "
+                  f"avg l. factor : {(linear_factor_sum / 10000.0):10.5f}")
+            linear_factor_sum = 0.0
             last_tstamp = now_tstamp
 
         if len(found_primes) >= 1000000:
